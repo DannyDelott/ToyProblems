26f403afc52055b5de58ad17c73748a3e85447bf
dannydelott <dannydelott@gmail.com>
2015-07-24 18:34:29 -0700

    Fix whitespace in evenOccurrence

--- evenOccurrence/evenOccurrence.py
+++ evenOccurrence/evenOccurrence.py
@@ -6,20 +6,20 @@
 
 def evenOccurrence( array ):
 
-  # Build a boolean dictionary of the numbers in the array and if they're even
-  def reduceEvens(dictionary, number):
-    dictionary[number] = not dictionary[number] if number in dictionary else False
-    return dictionary
-  evens = reduce(reduceEvens, array, {})
+    # Build a boolean dictionary of the numbers in the array and if they're even
+    def reduceEvens(dictionary, number):
+        dictionary[number] = not dictionary[number] if number in dictionary else False
+        return dictionary
+    evens = reduce(reduceEvens, array, {})
 
-  # Reduce the dictionary to the first even value of the array
-  def reducefirstEvenOccurrence(first, number):
-    first = number if first == None and evens[number] == True else first
-    return first
-  firstEvenOccurrence = reduce(reducefirstEvenOccurrence, array, None)
+    # Reduce the dictionary to the first even value of the array
+    def reducefirstEvenOccurrence(first, number):
+        first = number if first == None and evens[number] == True else first
+        return first
+    firstEvenOccurrence = reduce(reducefirstEvenOccurrence, array, None)
 
-  # Return the first even occurence in the array
-  return firstEvenOccurrence
+    # Return the first even occurence in the array
+    return firstEvenOccurrence
 
 # First Even Occurrence: 4
 print "First Even Occurrence:\t" + str(evenOccurrence([1, 2, 3, 3, 4, 4, 3, 5, 5]))
